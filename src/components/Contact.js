0b565a7b6830c90cdad24faf5186b8e1e31dbbe3
her97 <simkoon0120@gmail.com>
2024-07-01 22:36:03 +0900

    add

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
                 <p className="section-subtitle">How can you communicate?</p>
                 <h6 className="section-title mb-5">Contact Me</h6>
                 {/* contact form */}
-                <form action="" className="contact-form col-md-10 col-lg-8 m-auto">
+                <form action={`mailto:hermes970120@naver.com`} method="post" encType="text/plain" className="contact-form col-md-10 col-lg-8 m-auto">
                     <div className="form-row">
                         <div className="form-group col-sm-6">
                             <input type="text" size="50" className="form-control" placeholder="Your Name" required />
